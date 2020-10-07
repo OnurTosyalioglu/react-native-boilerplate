590a8563359c81ccd6699c3cb7f432a9f52fd63a
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-07 20:10:20 +0300

    [REFACTOR] tabbar moved to tabs component for semantic

--- src/page/home/home/Home.js
+++ src/page/home/home/Home.js
@@ -6,11 +6,6 @@ import {
 import styles from './styles'
 import props from './props'
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import tabIndex from '../../../route/tabIndex'
-
-const { Navigator, Screen } = createBottomTabNavigator()
-
 const Home = (props) => {  
 	return <View>
 	</View>
